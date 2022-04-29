6f05e26bae893d181cdfa372aea6b028e7c1281c
FrancoFantomius <56490563+FrancoFantomius@users.noreply.github.com>
2022-04-29 23:27:43 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,18 +1,11 @@
-self.addEventListener("install", e =>{
-    e.waitUntil(
-            caches.open("static").then(cache => {
-                return cache.addAll(["/",
-                "/src/index.css",
-                "/src/index.js",
-                "/img/logo-512.png",
-                "/img/cancel.svg",
-                "/img/more.svg",
-                "/img/√.svg",
-                "/index.html",
-                "https://fonts.googleapis.com/css2?family=Inter&display=swap"]);
-            }
-        )
-    )
+const urlsToCache = ["/", "index.html"];
+
+self.addEventListener("install", event => {
+   event.waitUntil(
+      caches.open("pwa-assets").then(cache => {
+         return cache.addAll(urlsToCache);
+      })
+   );
 });
 
 self.addEventListener("fetch", e => {
